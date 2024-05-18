16a7a641f30bdfa5055abc48c140d35e337c4bba
osoLife <ilovecode@foxmail.com>
2024-05-18 10:45:27 +0800

    添加cookie和session的处理

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
 const router = require('./router')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
+app.use(cookieSession({
+    name: 'mySession',
+    keys: ['skslsldkslkdkskdklsl'], // 混淆的字符串（加密）
+    maxAge: 60 * 1000
+}))
 app.use(express.static('./public'))
 app.use('/api', router)
 
--- router/index.js
+++ router/index.js
@@ -5,6 +5,32 @@ const secretKey = 'osoLife key'
 
 const router = express.Router()
 
+router.get('/setCookie', (req, res) => {
+    res.cookie('name', 'osoLife', {
+        maxAge: 1000 * 60
+    })
+    res.cookie('age', 38)
+    res.send('设置cookie')
+})
+
+router.get('/getCookie', (req, res) => {
+    const name = req.cookies['name']
+    const age = req.cookies['age']
+    res.send('获取到的cookie：' + name + ' ' + age)
+})
+
+router.get('/setSession', (req, res) => {
+    req.session['height'] = 1.65
+    req.session['address'] = 'china'
+    res.send('设置session')
+})
+
+router.get('/getSession', (req, res) => {
+    let height = req.session['height']
+    let address = req.session['address']
+    res.send('获取到的session：' + height + ' ' + address)
+})
+
 router.post('/login', (req, res) => {
     const token = jwt.sign({ name: 'osoLife' }, secretKey, { expiresIn: '0.5h' })
     res.send({
